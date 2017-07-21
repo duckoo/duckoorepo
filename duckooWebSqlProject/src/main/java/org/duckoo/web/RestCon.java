a6c0846b98278e90958c60d890b90f7d2ec9843d
taehyun <taehyun@DESKTOP-GI51RAJ>
2017-07-21 17:42:02 +0900

    fgh

--- duckooWebSqlProject/src/main/java/org/duckoo/web/RestCon.java
+++ duckooWebSqlProject/src/main/java/org/duckoo/web/RestCon.java
@@ -107,9 +107,9 @@ public class RestCon {
 			try {	
 				in= new FileInputStream(file);
 				String str = IOUtils.toString(in,"utf-8");
-				if(str.startsWith("\uFEFF")){
-					str=str.substring(1);
-				}
+//				if(str.startsWith("\uFEFF")){
+//					str=str.substring(1);
+//				}
 				ret.add(str);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
